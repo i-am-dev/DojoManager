15ad106de876fc80996910c9b0796d180dc5e542
i-am-dev <ace-dev@outlook.com>
2017-05-16 14:21:41 +0200

    Initial Commit
    Signed-off-by: i-am-dev <ace-dev@outlook.com>

---
+++ DojoManager/Data/DBManager.cs
@@ -0,0 +1,50 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DojoManager.Models;
+
+namespace DojoManager.Data
+{
+    public class DBManager
+    {
+        public string ConnectionString { get; set; }
+
+        public DBManager(string connectionString)
+        {
+            this.ConnectionString = connectionString;
+        }
+
+        private MySqlConnection GetConnection()
+        {
+            return new MySqlConnection(ConnectionString);
+        }
+
+        public List<User> GetAllUsers()
+        {
+            List<User> list = new List<User>();
+
+            using (MySqlConnection conn = GetConnection())
+            {
+                conn.Open();
+                MySqlCommand cmd = new MySqlCommand("SELECT * FROM Users", conn);
+                using (MySqlDataReader reader = cmd.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        list.Add(new User()
+                        {
+                            UserID = reader.GetInt32("ID"),
+                            Name = reader.GetString("Name"),
+                            Password = reader.GetString("Password"),
+                            Email = reader.GetString("Email")
+                        });
+                    }
+                }
+            }
+
+            return list;
+        }
+    }
+}
