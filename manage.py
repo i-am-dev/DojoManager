8a629c395d27184b935fd5c1f0c097b602a4137a
Richard <i-am-dev@users.noreply.github.com>
2019-04-07 20:37:15 +0200

    Add in code coverage (#35)
    * Added code coverage
    
    * Added code coverage
    
    * Added code coverage
    
    * Added code coverage

--- manage.py
+++ manage.py
@@ -1,5 +1,6 @@
 import os
 import unittest
+import coverage
 
 from flask_migrate import Migrate, MigrateCommand
 from flask_script import Manager
@@ -7,6 +8,10 @@ from flask_script import Manager
 from app import blueprint
 from app.main import create_app, db
 from app.main.model import user, blacklist
+
+
+
+
 
 app = create_app(os.getenv('BOILERPLATE_ENV') or 'dev')
 app.register_blueprint(blueprint)
@@ -23,6 +28,40 @@ manager.add_command('db', MigrateCommand)
 @manager.command
 def run():
     app.run()
+
+
+@manager.command
+def cov():
+    """
+    Runs the unit tests and generates a coverage report on success.
+    While the application is running, you can run the following command in a new terminal:
+    'docker-compose run --rm flask python manage.py cov' to run all the tests in the
+    'tests' directory. If all the tests pass, it will generate a coverage report.
+    :return int: 0 if all tests pass, 1 if not
+    """
+
+    # Defines which parts of the code to include and omit when calculating code coverage.
+    COV = coverage.coverage(
+        branch=True,
+        include='app/main/*',
+        omit=[
+            'app/test/*'
+        ],
+        data_file='.coverage'
+    )
+    COV.start()
+    tests = unittest.TestLoader().discover('app/test', pattern='test*.py')
+    result = unittest.TextTestRunner(verbosity=2).run(tests)
+    if result.wasSuccessful():
+        COV.stop()
+        COV.save()
+        print('Coverage Summary:')
+        COV.report()
+        #COV.html_report()
+        #COV.erase()
+        return 0
+    else:
+        return 1
 
 
 @manager.command
